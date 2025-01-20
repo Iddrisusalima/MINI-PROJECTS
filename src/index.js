5f1382b23c7b7ef34cac58732550a372553b05cc
JacklinPrempeh <Jackiep6132@gmail.com>
2025-01-20 19:00:33 +0000

    adding changes

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ on6.addEventListener("mouseover",changesix);
 
 function change6(){
     let get =document.getElementById("stat");
-    get.style.backgroundColor="white";
+    get.style.backgroundColor="#004080";
     get.style.transitionDuration="300ms";
 }
 
